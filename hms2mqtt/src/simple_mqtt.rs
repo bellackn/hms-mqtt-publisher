8efc6480184381e59dcdef76ea5b5b68de9bca32
bellackn <blcknc@pm.me>
2024-04-09 22:24:36 +0200

    feat: put inverter_id into topic

--- hms2mqtt/src/simple_mqtt.rs
+++ hms2mqtt/src/simple_mqtt.rs
@@ -25,9 +25,9 @@ impl<MQTT: MqttWrapper> MetricCollector for SimpleMqtt<MQTT> {
     fn publish(&mut self, hms_state: &HMSStateResponse) {
         debug!("{hms_state}");
 
-        let inverter_id = hms_state.inverter_state[0].inv_id;
         let d = UNIX_EPOCH + Duration::from_secs(hms_state.time as u64);
         let datetime = DateTime::<Local>::from(d);
+        let inverter_id = hms_state.inverter_state[0].inv_id;
         let inverter_local_time = datetime.format("%Y-%m-%d %H:%M:%S.%f").to_string();
 
         let pv_current_power = hms_state.pv_current_power as f32 / 10.;
@@ -48,32 +48,76 @@ impl<MQTT: MqttWrapper> MetricCollector for SimpleMqtt<MQTT> {
 
         // TODO: this section bears a lot of repetition. Investigate if there's a more idiomatic way to get the same result, perhaps using a macro
         let topic_payload_pairs = [
-            ("hms800wt2/inverter_id", inverter_id.to_string()),
-            ("hms800wt2/inverter_local_time", inverter_local_time),
-            ("hms800wt2/pv_current_power", pv_current_power.to_string()),
-            ("hms800wt2/pv_daily_yield", pv_daily_yield.to_string()),
-            ("hms800wt2/pv_current_power", pv_current_power.to_string()),
-            ("hms800wt2/pv_daily_yield", pv_daily_yield.to_string()),
-            ("hms800wt2/pv_grid_voltage", pv_grid_voltage.to_string()),
-            ("hms800wt2/pv_grid_freq", pv_grid_freq.to_string()),
             (
-                "hms800wt2/pv_inv_temperature",
+                format!("hms800wt2/{inverter_id}/inverter_local_time"),
+                inverter_local_time,
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_current_power"),
+                pv_current_power.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_daily_yield"),
+                pv_daily_yield.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_current_power"),
+                pv_current_power.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_daily_yield"),
+                pv_daily_yield.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_grid_voltage"),
+                pv_grid_voltage.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_grid_freq"),
+                pv_grid_freq.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_inv_temperature"),
                 pv_inv_temperature.to_string(),
             ),
-            ("hms800wt2/pv_port1_voltage", pv_port1_voltage.to_string()),
-            ("hms800wt2/pv_port1_curr", pv_port1_curr.to_string()),
-            ("hms800wt2/pv_port1_power", pv_port1_power.to_string()),
-            ("hms800wt2/pv_port1_energy", pv_port1_energy.to_string()),
             (
-                "hms800wt2/pv_port1_daily_yield",
+                format!("hms800wt2/{inverter_id}/pv_port1_voltage"),
+                pv_port1_voltage.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_port1_curr"),
+                pv_port1_curr.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_port1_power"),
+                pv_port1_power.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_port1_energy"),
+                pv_port1_energy.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_port1_daily_yield"),
                 pv_port1_daily_yield.to_string(),
             ),
-            ("hms800wt2/pv_port2_voltage", pv_port2_voltage.to_string()),
-            ("hms800wt2/pv_port2_curr", pv_port2_curr.to_string()),
-            ("hms800wt2/pv_port2_power", pv_port2_power.to_string()),
-            ("hms800wt2/pv_port2_energy", pv_port2_energy.to_string()),
             (
-                "hms800wt2/pv_port2_daily_yield",
+                format!("hms800wt2/{inverter_id}/pv_port2_voltage"),
+                pv_port2_voltage.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_port2_curr"),
+                pv_port2_curr.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_port2_power"),
+                pv_port2_power.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_port2_energy"),
+                pv_port2_energy.to_string(),
+            ),
+            (
+                format!("hms800wt2/{inverter_id}/pv_port2_daily_yield"),
                 pv_port2_daily_yield.to_string(),
             ),
         ];
